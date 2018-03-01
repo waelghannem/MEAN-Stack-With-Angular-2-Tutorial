d9c1ba460ad3489ec5e68dc8f48b53375fa6e889
waelghannem <waelghannem@gmail.com>
2018-03-01 10:37:49 +0100

    server-setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+var mongoose = require('mongoose');
+const config = require('./config/database');
+const path = require('path');
+
+mongoose.Promise=global.Promise;
+
+
+mongoose.connect(config.uri,(err)=>{
+    if(err){
+        console.log('could not connect to database'+err);
+    }else {
+        console.log('connected to db :'+config.db);
+    }
+});
+
+app.use(express.static(__dirname+'/client/dist/'))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname+"/client/dist/index.html"));
+});
+
+app.listen(8080, ()=>{
+    console.log('server is listening on port 8080');
+});
